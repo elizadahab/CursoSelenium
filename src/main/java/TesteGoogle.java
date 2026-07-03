082ea992a73a35cab9dd14ab4eeaecb004ff5bc5
elizadahab <ecdahab@gmail.com>
2026-07-03 14:50:30 -0300

    Feat: teste automatizado utilizando o framework Junit

---
+++ src/main/java/TesteGoogle.java
@@ -0,0 +1,15 @@
+import org.junit.Assert;
+import org.junit.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class TesteGoogle {
+	
+	@Test
+	public void teste() {
+		WebDriver driver = new FirefoxDriver();
+		driver.get("http://www.google.com");
+		Assert.assertEquals("Google", driver.getTitle());
+
+	}
+}
