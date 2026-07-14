cdf391ba8ef7f5c7ca739bd02185353fc044b6a7
elizadahab <ecdahab@gmail.com>
2026-07-14 13:36:23 -0300

    test: implementar testes para o fluxo completo de cadastro

--- src/main/java/TesteCadastro.java
+++ src/main/java/TesteCadastro.java
@@ -1,50 +1,37 @@
 import org.junit.Assert;
 import org.junit.Test;
 import org.openqa.selenium.By;
-import org.openqa.selenium.Dimension;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.ui.Select;
 
 public class TesteCadastro {
 
 	@Test
-	public void testeNome() {
+	public void testeCadastrar() {
 		WebDriver driver = new FirefoxDriver();
 		driver.get("file:///" + System.getProperty("user.dir") + "/src/main/resources/componentes.html");
+		
 		driver.findElement(By.id("elementosForm:nome")).sendKeys("Elizabeth");
-		Assert.assertEquals("Elizabeth", driver.findElement(By.id("elementosForm:nome")).getAttribute("value"));
-		// driver.quit();
-	}
-
-	@Test
-	public void testeSobrenome() {
-		WebDriver driver = new FirefoxDriver();
-		driver.get("file:///" + System.getProperty("user.dir") + "/src/main/resources/componentes.html");
 		driver.findElement(By.id("elementosForm:sobrenome")).sendKeys("Dahab");
-		Assert.assertEquals("Dahab", driver.findElement(By.id("elementosForm:nome")).getAttribute("value"));
-		// driver.quit();
-	}
-
-	@Test
-	public void testSexo() {
-
-		WebDriver driver = new FirefoxDriver();
-		driver.get("file:///" + System.getProperty("user.dir") + "/src/main/resources/componentes.html");
 		driver.findElement(By.id("elementosForm:sexo:1")).click();
-		Assert.assertTrue(driver.findElement(By.id("elementosForm:sexo:1")).isSelected());
-		// driver.quit();
-	}
-
-	@Test
-	public void testComidaFavorita() {
-
-		WebDriver driver = new FirefoxDriver();
-		driver.get("file:///" + System.getProperty("user.dir") + "/src/main/resources/componentes.html");
 		driver.findElement(By.id("elementosForm:comidaFavorita:2")).click();
-		Assert.assertTrue(driver.findElement(By.id("elementosForm:comidaFavorita:2")).isSelected());
-		//driver.quit();
+		new Select(driver.findElement(By.id("elementosForm:escolaridade"))).selectByVisibleText("Mestrado");
+		new Select(driver.findElement(By.id("elementosForm:esportes"))).selectByVisibleText("Corrida");
+		driver.findElement(By.id("elementosForm:sugestoes")).sendKeys("Eu faço crossfit!");
+		driver.findElement(By.id("elementosForm:cadastrar")).click();
+		
+		// confirma que os elementos foram cadastrados
+		Assert.assertTrue(driver.findElement(By.id("resultado")).getText().startsWith("Cadastrado!"));
+		Assert.assertTrue(driver.findElement(By.id("descNome")).getText().endsWith("Elizabeth"));
+		Assert.assertTrue(driver.findElement(By.id("descSobrenome")).getText().endsWith("Dahab"));
+		Assert.assertTrue(driver.findElement(By.id("descSexo")).getText().endsWith("Feminino"));
+		Assert.assertTrue(driver.findElement(By.id("descComida")).getText().endsWith("Pizza"));
+		Assert.assertEquals("Escolaridade: mestrado", driver.findElement(By.id("descEscolaridade")).getText());
+		Assert.assertEquals("Esportes: Corrida", driver.findElement(By.id("descEsportes")).getText());
+		Assert.assertEquals("Sugestoes: Eu faço crossfit!", driver.findElement(By.id("descSugestoes")).getText());
+		
+		driver.quit();
+		
 	}
-	
-	@Test
-	
 }
